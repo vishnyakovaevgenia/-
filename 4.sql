6171f677ec2d2909983bc41454be8d3580bc7a1d
vishnyakovaevgenia <108328330+vishnyakovaevgenia@users.noreply.github.com>
2022-07-02 09:51:29 +0300

    Update and rename new.sql to 4.sql

---
+++ 4.sql
@@ -0,0 +1,14 @@
+
+creat table `new sportsman` (
+  `id` int not null,
+  `Name` varchar(100) not null,
+  `Phone` varchar(11) not null,
+  `Bith_date` date not null,
+  `Biography` text null,
+  `Video` blog null
+);
+
+SELECT 
+`Name` AS 'Участник',
+(SELECT COUNT(*) FROM `result` WHERE `sportsman`.`ID`=`result`.`Name`) AS 'Количество соревнований'
+FROM `sportsman` ORDER BY (SELECT COUNT(*) FROM `result` WHERE `sportsman`.`ID`=`result`.`Name`) DESC LIMIT 5;
